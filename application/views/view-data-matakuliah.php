f3c1d65413d77a57b6814ffce5ec3143b4569e07
zenoxide <12200015@bsi.ac.id>
2021-10-01 15:53:52 +0700

    Penambahan File matakuliah.php, view-form-matakuliah.php, view-data-matakuliah.php

--- application/views/view-data-matakuliah.php
+++ application/views/view-data-matakuliah.php
@@ -1,15 +1,12 @@
 <html>
 <head>
-41
  <title>Tampil Data Matakuliah</title>
 </head>
 <body>
  <center>
  <table>
  <tr>
- <th colspan="3">
- Tampil Data Mata Kuliah
- </th>
+ <th colspan="3"> Tampil Data Mata Kuliah</th>
  </tr>
  <tr>
  <td colspan="3">
@@ -19,28 +16,22 @@
  <tr>
  <th>Kode MTK</th>
  <th>:</th>
- <td>
- <?= $kode; ?>
- </td>
+ <td> <?= $kode; ?></td>
  </tr>
  <tr>
  <td>Nama MTK</td>
  <td>:</td>
- <td>
- <?= $nama; ?>
- </td>
+ <td><?= $nama; ?></td>
  </tr>
  <tr>
  <td>SKS</td>
  <td>:</td>
- <td>
- <?= $sks; ?>
- </td>
+ <td><?= $sks; ?></td>
  </tr>
  <tr>
  <td colspan="3" align="center">
  <a href="<?= base_url('matakuliah');
-?>">Kembali</a>
+ ?>">Kembali</a>
  </td>
  </tr>
  </table>
--- application/views/view-form-matakuliah.php
+++ application/views/view-form-matakuliah.php
@@ -4,13 +4,10 @@
 </head>
 <body>
  <center>
- <form action="<?= base_url('matakuliah'); ?>"
-method="post">
+ <form action="<?= base_url('matakuliah/cetak'); ?>" method="post">
  <table>
  <tr>
- <th colspan="3">
- Form Input Data Mata Kuliah
- </th>
+ <th colspan="3"> Form Input Data Mata Kuliah </th>
  </tr>
  <tr>
  <td colspan="3">
@@ -20,22 +17,17 @@ method="post">
  <tr>
  <th>Kode MTK</th>
  <th>:</th>
- <td>
- <input type="text" name="kode" id="kode">
- </td>
+ <td> <input type="text" name="kode" id="kode"> </td>
  </tr>
  <tr>
  <th>Nama MTK</th>
  <td>:</td>
- <td>
- <input type="text" name="nama" id="nama">
- </td>
+ <td> <input type="text" name="nama" id="nama"> </td>
  </tr>
  <tr>
  <th>SKS</th>
  <td>:</td>
- <td>
- <select name="sks" id="sks">
+ <td> <select name="sks" id="sks">
  <option value="">Pilih SKS</option>
  <option value="2">2</option>
  <option value="3">3</option>
